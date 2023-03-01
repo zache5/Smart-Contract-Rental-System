87b0f710bb5ab9cfe67011e2c8e891bdab6897c9
robin thorsen <robinbthorsen@gmail.com>
2023-02-28 17:32:34 -1000

    readding my files

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd 
+import datetime 
 
 load_dotenv()
 
@@ -66,53 +67,89 @@ def get_fleet_data():
 # something where we have a simple home pager with that, and seperate pages displaying what 
 # the renter, and business side will look like. 
 ################################################################################
-
-st.title("Welcome to our rental system using smart contracts!")
-st.write("Are you a business owner or a renter?")
-
-st.write("Business Owner")
-st.write("You are a business owner")
-vin = st.text_input("Enter the VIN of the vehicle")
-make = st.text_input("Enter the make of the vehicle")
-model = st.text_input("Enter the model of the vehicle")
-license_plate = st.text_input("Enter the license plate of the vehicle")
-year = st.number_input("Enter the year of the vehicle", min_value=1900, max_value=2100)
-stock_name = st.text_input("Enter the stock name of the vehicle")
-daily_price = int(st.number_input("Enter the daily rental price of the vehicle"))
-# address = st.text_input("Enter the address to mint the vehicle with")
-
-if st.button("Add Vehicle"):
-    # Check if the VIN or license plate already exists in the fleet
-    # existing_vehicles = NFT_contract.functions.getFleet().call()
-    # for vehicle_id in existing_vehicles:
-    #     vehicle_details = NFT_contract.functions.getVehicleDetails(vehicle_id).call()
-    #     if vehicle_details[0] == vin or vehicle_details[3] == license_plate:
-    #         st.error("Error: A vehicle with the same VIN or license plate already exists in the fleet.")
-        
-
-    # Add the vehicle to the fleet
-    tx_hash = NFT_contract.functions.createVehicleNFT(vin, make, model, license_plate, year, stock_name, daily_price).transact({'from': address, 'gas': 1000000})
-    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    st.write("Transaction receipt mined:")
-    st.write(dict(receipt))
-
-    # Get the ID of the newly minted vehicle
-    event_filter = NFT_contract.events.VehicleNFTCreated.createFilter(fromBlock="latest")
-    events = event_filter.get_all_entries()
-    new_vehicle_id = events[-1]['args']['tokenId']
-
-    # Show the user the newly minted vehicle
-    st.write("Vehicle added to your fleet:")
-    # st.image(NFT_contract.functions.tokenURI(new_vehicle_id).call())
-
-if st.button("Renter"):
-    st.write("You are a renter")
-    # Display rental options and allow the renter to select a vehicle to rent
-st.write(os.getenv("NFT_CONTRACT_ADDRESS"))    
+def intro():
+    st.title("Welcome to our rental system using smart contracts!")
+    st.write("Are you a business owner or a renter?")
+    st.write("Click on corresponding page on the left.")
     
+  
+    st.write(os.getenv("NFT_CONTRACT_ADDRESS"))    
 
-if st.button("Check Availability"):
-    vehicle_details_df = get_fleet_data()
-    st.write(vehicle_details_df)
-        
+
+    if st.button("Check Availability"):
+        vehicle_details_df = get_fleet_data()
+        st.write(vehicle_details_df)
+
+def business():
+    st.title('Welcome to our rental system! please add some business information')
+   
     
+    st.write("Lets add some vehicles to your virtual fleet, start by adding the information assiociated with a vehicle")
+    vin = st.text_input("Enter the VIN of the vehicle")
+    make = st.text_input("Enter the make of the vehicle")
+    model = st.text_input("Enter the model of the vehicle")
+    license_plate = st.text_input("Enter the license plate of the vehicle")
+    year = st.number_input("Enter the year of the vehicle", min_value=1900, max_value=2100)
+    stock_name = st.text_input("Enter the stock name of the vehicle")
+    daily_price = int(st.number_input("Enter the daily rental price of the vehicle"))
+    
+    if st.button("Add Vehicle"):
+        # Check if the VIN or license plate already exists in the fleet
+        # existing_vehicles = NFT_contract.functions.getFleet().call()
+        # for vehicle_id in existing_vehicles:
+        #     vehicle_details = NFT_contract.functions.getVehicleDetails(vehicle_id).call()
+        #     if vehicle_details[0] == vin or vehicle_details[3] == license_plate:
+        #         st.error("Error: A vehicle with the same VIN or license plate already exists in the fleet.")
+
+
+        # Add the vehicle to the fleet
+        tx_hash = NFT_contract.functions.createVehicleNFT(vin, make, model, license_plate, year, stock_name, daily_price).transact({'from': address, 'gas': 1000000})
+        receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+        st.write("Transaction receipt mined:")
+        st.write(dict(receipt))
+
+        # Get the ID of the newly minted vehicle
+        event_filter = NFT_contract.events.VehicleNFTCreated.createFilter(fromBlock="latest")
+        events = event_filter.get_all_entries()
+        new_vehicle_id = events[-1]['args']['tokenId']
+
+        # Show the user the newly minted vehicle
+        st.write("Vehicle added to your fleet:")
+        # st.image(NFT_contract.functions.tokenURI(new_vehicle_id).call())
+    st.write(os.getenv("NFT_CONTRACT_ADDRESS"))  
+    
+def renter():
+    st.title("Welcome to our rental system using smart contracts!")
+    
+    st.write("Please enter your information below to get started.")
+    
+    # Get renter information
+    first_name = st.text_input("First Name")
+    last_name = st.text_input("Last Name")
+    email = st.text_input("Email")
+    phone_number = st.text_input("Phone Number")
+    
+    st.write("Please select the dates you would like to rent:")
+    
+    # Display a calendar for selecting rental dates
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
+    
+    # Convert selected dates to Unix timestamps
+    start_unix = int(datetime.datetime.combine(start_date, datetime.time.min).timestamp())
+    end_unix = int(datetime.datetime.combine(end_date, datetime.time.min).timestamp())
+    
+    # Display renter information and selected rental dates
+    st.write(f"Name: {first_name} {last_name}")
+    st.write(f"Email: {email}")
+    st.write(f"Phone Number: {phone_number}")
+    st.write(f"Rental Dates: {start_unix} to {end_unix}")
+    
+page_names = {
+    'Home Page': intro,
+    'Business': business,
+    'Renter':renter
+}
+
+page = st.sidebar.selectbox("Pages", page_names.keys())
+page_names[page]()
